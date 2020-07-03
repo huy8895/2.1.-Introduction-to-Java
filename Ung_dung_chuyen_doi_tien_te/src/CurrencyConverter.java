8b7f0b3952d835f2c4bd41618bbce7d2c29513be
huy8895 <huy8895@gmail.com>
2020-07-03 11:41:00 +0700

    bai tap ung dung chuyen doi tien te

--- Ung_dung_chuyen_doi_tien_te/src/CurrencyConverter.java
+++ Ung_dung_chuyen_doi_tien_te/src/CurrencyConverter.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class CurrencyConverter {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so tien USD :");
+        usd = sc.nextDouble();
+        double quydoi = usd * vnd;
+        System.out.print("gia tri VND: " + quydoi);
+
+    }
 }
